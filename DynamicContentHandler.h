f22cc93d7313352f973fc498743bf7954390a40f
fzw1316 <147839209+fzw1316@users.noreply.github.com>
2024-12-29 14:55:48 +0800

    Update DynamicContentHandler.h

--- DynamicContentHandler.h
+++ DynamicContentHandler.h
@@ -1,1 +1,12 @@
+#include <ctime>
+#include <string>
+#include <iostream>
 
+class echoServer {
+public:
+    void HandleMessage(Connection conn, std::string& message);
+
+private:
+    bool isDynamicRequest(const std::string& message);
+    std::string generateDynamicContent(const std::string& request);
+};
